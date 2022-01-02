3e5e36e5b8be99760e353e1753474523863695a2
Dorian Wysocki <dorian.wysocki@web24.com.pl>
2022-01-02 21:40:00 +0100

    memory offsets

---
+++ cs/offsets.go
@@ -0,0 +1,9 @@
+package cs
+
+const dwEntityList = 0x4DD245C
+const dwLocalPlayer = 0xDB65DC
+const dwGlowObjectManager = 0x531B048
+const m_iHealth = 0x100
+const m_iTeamNum = 0xF4
+const m_vecOrigin = 0x138
+const m_iGlowIndex = 0x10488
